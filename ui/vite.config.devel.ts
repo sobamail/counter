22bfe0a5309f39cb56cb89cf460a4e9794a61e7b
Burak Arslan <burak.arslan@arskom.com.tr>
2025-11-07 16:50:34 +0300

    Create npm run dev-watch command

---
+++ ui/vite.config.devel.ts
@@ -0,0 +1,11 @@
+import {svelte} from "@sveltejs/vite-plugin-svelte"
+import {defineConfig} from "vite"
+
+export default defineConfig({
+    plugins : [ svelte() ],
+    build : {
+        minify : false,
+        outDir : "../assets",
+        emptyOutDir : true,
+    },
+});
